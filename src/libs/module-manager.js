3f07c0ff0b02cf9a499628ab8fb8ed28d2a8760c
李晶 <lijing@bingosoft.net>
2018-12-29 17:41:27 +0800

    添加异步模块库注册功能

--- src/libs/module-manager.js
+++ src/libs/module-manager.js
@@ -1,6 +1,30 @@
-var mods=[];
+var mods=[],asyncMods={};
+//同步添加模块库
 function add(mod){
     mods.push(mod);
+}
+//异步添加模块库
+function addAsync(modName,mod,appCtx,homeBase,pageIndex){
+    if((!!modName) && (!!mod)){
+        mods.push(mod);
+        asyncMods[modName]=mod;
+        if(mod.pages&&mod.pages.routes&&appCtx&&appCtx.getRouter){
+            let modRoutes=_.cloneDeep(homeBase);
+            mod.pages.routes.forEach(route=>{
+                if(!route.component){
+                    route.component=pageIndex;
+                }
+            });
+            modRoutes.children=mod.pages.routes;
+            appCtx.getRouter().addRoutes([modRoutes]);
+        }
+
+        mod.initAfterAppCtxCreated && 
+        mod.initAfterAppCtxCreated(appCtx);
+
+        mod.initAfterAppStarted && 
+        mod.initAfterAppStarted(appCtx);
+    }
 }
 function initAfterAppCtxCreated(appCtx){
     mods.forEach(mod => {
@@ -14,9 +38,24 @@ function initAfterAppStarted(appCtx){
         mod.initAfterAppStarted(appCtx);
     });
 }
+function isAsyncModLoaded(routePath){
+    if(!routePath){
+        return false;
+    }
+    for (const key in asyncMods) {
+        if (asyncMods.hasOwnProperty(key)) {
+            if(routePath.indexOf(`/pages/${key}/`)>-1){
+                return true;
+            }
+        }
+    }
+    return false;
+}
 export default {
     add,
     initAfterAppCtxCreated,
     initAfterAppStarted,
-    mods
+    mods,
+    addAsync,
+    isAsyncModLoaded
 }
